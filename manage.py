1cc3194d56245544fabf3ca69e7705d7b46e09f3
sunildale <118259186+sunildale@users.noreply.github.com>
2023-07-18 15:37:11 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+3#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -20,3 +20,4 @@ def main():
 if __name__ == '__main__':
     main()
 added some change 2
+add 2.1
